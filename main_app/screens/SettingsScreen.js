e129197c3954d06c8b2ce112cdec7e98607dbb8e
Shriya Dale <shriya.dale@outlook.com>
2024-03-27 17:11:22 -0400

    fixed janky code bc of joe ;((((

---
+++ main_app/screens/SettingsScreen.js
@@ -0,0 +1,39 @@
+import * as React from 'react'; 
+import {View, Text} from 'react-native'; 
+
+export default function SettingsScreen({navigation}){
+    return(
+        <View style={styles.screenBasic}>
+            <Text 
+                onPress={() => navigation.navigate('Home')}
+                style = {styles.alerts}>Settings Screen</Text>
+        </View>
+    );
+} 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0F172A',
+        position: 'relative', 
+    },
+    backgroundImage: {
+        position: 'absolute',
+        height: '100%',
+        width: '100%',
+    },
+    screenBasic: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    alerts:{
+        fontSize: 26,
+        fontWeight: 'bold',
+    },
+    contentContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+});
